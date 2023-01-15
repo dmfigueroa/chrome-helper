cac8f17d8404a809d4a540988105044b56540101
David Figueroa <davidfigueroa0327@gmail.com>
2023-01-15 08:32:15 -0500

    open in dark mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::process::Command;
+
+fn get_color_scheme() -> String {
+    let mut gsettings = Command::new("color-schema");
+
+    let output = gsettings.output().expect("Failed to get color scheme");
+    let color_scheme =
+        String::from_utf8(output.stdout).expect("Failed to convert color scheme to string");
+    color_scheme.trim().replace("\"", "").to_string()
+}
+
+fn open_chrome() {
+    let mut args = vec!["--restore-last-session"];
+    // Print color scheme
+    let color_scheme = get_color_scheme();
+    if color_scheme.eq("dark") {
+        args.push("--force-dark-mode");
+    }
+    Command::new("google-chrome-stable").args(args).spawn().expect("Failed to open Chrome");
+}
+
 fn main() {
-    println!("Hello, world!");
+    open_chrome();
 }
